ea8cc5113230cbee9e77682ead9d3c6d08df3eb7
Danilo <danilo_lgd@hotmail.com>
2014-11-17 07:58:19 -0200

    Ajustes das Classes

--- src/bazar/ClienteDAO.java
+++ src/bazar/ClienteDAO.java
@@ -7,7 +7,10 @@ package bazar;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -85,6 +88,79 @@ public class ClienteDAO {
             ps.execute();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    public List<Cliente> selecionarTodos() {
+        List<Cliente> listac = new ArrayList();
+        String sql = "SELECT * FROM Cliente";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery(sql);
+
+            while (rs.next()) {
+                listac.add(populacliente(rs));
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return listac;
+    }
+
+    public List<Cliente> selecionarTodos(boolean ativos) {
+        List<Cliente> listac = new ArrayList();
+        if (ativos) {
+            String sql = "SELECT * FROM Cliente WHERE status = 1";
+            try {
+                PreparedStatement ps = con.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery(sql);
+
+                while (rs.next()) {
+                    listac.add(populacliente(rs));
+                }
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        } else {
+            String sql = "SELECT * FROM Cliente WHERE status = 0";
+            try {
+                PreparedStatement ps = con.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery(sql);
+
+                while (rs.next()) {
+                    listac.add(populacliente(rs));
+                }
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return listac;
+    }
+
+    private Cliente populacliente(ResultSet linha) {
+        Cliente c = new Cliente();
+        try {
+            c.setCodigo(linha.getInt("codigo"));
+            c.setNome(linha.getString("nome"));
+            c.setSobrenome(linha.getString("sobrenome"));
+            c.setGenero(linha.getString("genero"));
+            c.setCpf(linha.getString("cpf"));
+            c.setRg(linha.getString("rg"));
+            c.setEmail(linha.getString("email"));
+            c.setTelefone(linha.getString("telefone"));
+            c.setCelular(linha.getString("celular"));
+            c.setNascimento(linha.getString("nascimento"));
+            c.setStatus(linha.getInt("status"));
+            c.setDescricao(linha.getString("descricao"));
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            return c;
         }
     }
 
