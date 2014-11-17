c528563b82f55968dbe84225ded74bc5b7b57652
Danilo <danilo_lgd@hotmail.com>
2014-11-16 23:19:43 -0200

    Criação da Classe Categoria

---
+++ src/bazar/Categoria.java
@@ -0,0 +1,61 @@
+package bazar;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author danilo
+ */
+public class Categoria {
+    
+    private Integer codigo;
+    private String nome;
+    private Integer status;
+
+    /**
+     * @return the codigo
+     */
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the status
+     */
+    public Integer getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+    
+    
+}
