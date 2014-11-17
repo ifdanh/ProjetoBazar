362777920206fe3fbdf56c528b5c4fa892f8bd93
André Galdino <andre.galdinolima@ig.com.br>
2014-11-17 00:48:52 -0200

    Classe EnderecoDao, metodos salvar, alterar, deletear, selecionarTodos

---
+++ src/bazar/EnderecoDao.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bazar;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author andregaldino
+ */
+public class EnderecoDao {
+    private Connection con;
+    
+    public EnderecoDao() {
+        this.con = FabricaConexao.pegaConexao();
+    }
+    
+    public void salvar(Endereco  endereco){
+        String sql = "INSERT INTO Endereco (rua,numero,bairro,cidade,estado,cep,complemento)"
+                + " VALUES (?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, endereco.getRua());
+            ps.setInt(2, endereco.getNumero());
+            ps.setString(3, endereco.getBairro());
+            ps.setString(4, endereco.getCidade());
+            ps.setString(5, endereco.getEstado());
+            ps.setString(6, endereco.getCep());
+            ps.setString(7, endereco.getComplemento());
+            ps.execute();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void alterar(Endereco endereco){
+        String sql = "UPDATE Endereco set "
+                + "rua = ?,"
+                + "numero = ?, "
+                + "bairro = ?, "
+                + "cidade = ?, "
+                + "estado = ?, "
+                + "cep = ?, "
+                + "complemento = ? "
+                + " WHERE codigo = ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, endereco.getRua());
+            ps.setInt(2, endereco.getNumero());
+            ps.setString(3, endereco.getBairro());
+            ps.setString(4, endereco.getCidade());
+            ps.setString(5, endereco.getEstado());
+            ps.setString(6, endereco.getCep());
+            ps.setString(7, endereco.getComplemento());
+            ps.setInt(8, endereco.getCodigo());
+            ps.execute();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void deletar(Endereco endereco){
+        String sql = "DELETE FROM Perfil WHERE codigo = ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, endereco.getCodigo());
+            
+            ps.execute();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public List<Endereco> selecionarTodos(){
+        List<Endereco> listae = new ArrayList();
+        String sql = "SELECT * FROM Endereco";
+            try {
+                PreparedStatement ps = con.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery(sql);
+                
+                while (rs.next()) {                    
+                    listae.add(populaendereco(rs));
+                }
+                
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        return listae;
+    }
+    
+    
+   private Endereco populaendereco(ResultSet linha){
+       Endereco end = new Endereco();
+       try {
+           end.setRua(linha.getString("rua"));
+           end.setBairro(linha.getString("bairro"));
+           end.setCidade(linha.getString("cidade"));
+           end.setEstado(linha.getString("estado"));
+           end.setCep(linha.getString("cep"));
+           end.setComplemento(linha.getString("complemento"));
+           end.setNumero(linha.getInt("numero"));
+           end.setCodigo(linha.getInt("codigo"));
+           
+       } catch (SQLException e) {
+           System.out.println(e.getMessage());
+       }finally{
+           return end;
+       }       
+   }
+    
+}
